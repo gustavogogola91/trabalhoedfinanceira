d78376a61e6bb2256b965b5031e9ab66d1b32f0d
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2022-09-06 15:50:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function inicializa() {
+    document.addEventListener("keydown", () => {
+        const key = event.key
+        const urlAtual = window.location.href;
+        console.log(urlAtual)
+        
+        if (key == "ArrowRight") {
+            if (urlAtual = "")
+            window.location.replace()
+        }
+    })
+}
