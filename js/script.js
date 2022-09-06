c8557d5b58ca972c6061924c223447a15a8dd87d
Gustavo Gogola <90578528+gustavogogola91@users.noreply.github.com>
2022-09-06 17:39:16 -0300

    redirecionamento por meio das setas do teclado

--- js/script.js
+++ js/script.js
@@ -3,10 +3,30 @@ function inicializa() {
         const key = event.key
         const urlAtual = window.location.href;
         console.log(urlAtual)
-        
+
         if (key == "ArrowRight") {
-            if (urlAtual = "")
-            window.location.replace()
+            if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/graduacao.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/graduacao.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/valorCurso.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/valorCurso.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/pages/areasAtuacao.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/pages/areasAtuacao.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/pages/salario.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/pages/salario.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/")}
+        }
+        if (key == "ArrowLeft") {
+            if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/pages/salario.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/pages/salario.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/pages/areasAtuacao.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/pages/areasAtuacao.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/valorCurso.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/valorCurso.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/pages/graduacao.html")
+            } else if (urlAtual == "https://gustavogogola91.github.io/trabalhoedfinanceira/pages/graduacao.html") {
+                window.location.replace("https://gustavogogola91.github.io/trabalhoedfinanceira/")}
         }
     })
 }
